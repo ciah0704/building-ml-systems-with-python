d04b9bfe3c9630f7c7908514a9fe4e1f528fb2a5
nastra <etudenhoefner@gmail.com>
2014-01-04 13:23:52 +0100

    added method to determine an optimal cluster size for kmeans

--- clustering_related_posts.py
+++ clustering_related_posts.py
@@ -6,6 +6,7 @@ from nltk.stem import SnowballStemmer
 from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 import os
+import sys
 from sklearn import metrics
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -68,6 +69,72 @@ def get_similar_posts(X, post, posts):
     return None, None, None
 
 
+def bench_k_means(km, name, data):
+    print(120 * '=')
+    t0 = time()
+    km.fit(data)
+    print "Algorithm -- Time -- Homogeneity -- Completeness -- V-Measure -- Adjusted Rand Index -- Adjusted Mutual Info -- Silhouette Coefficient"
+    print(
+        '% 9s    %.2fs   %.3f           %.3f            %.3f         %.3f                   %.3f                    %.3f'
+        % (name, (time() - t0),
+           metrics.homogeneity_score(labels, km.labels_),
+           metrics.completeness_score(labels, km.labels_),
+           metrics.v_measure_score(labels, km.labels_),
+           metrics.adjusted_rand_score(labels, km.labels_),
+           metrics.adjusted_mutual_info_score(labels, km.labels_),
+           metrics.silhouette_score(data, km.labels_, metric='euclidean')))
+    print(120 * '=')
+
+
+def determine_best_cluster_size(min_clusters, max_clusters, data):
+    assert min_clusters < max_clusters
+    best_score = -1
+    cluster_size = max_clusters
+    for i in range(min_clusters, max_clusters):
+        km = KMeans(init='k-means++', n_clusters=i, n_init=10)
+        km.fit(data)
+        score = metrics.homogeneity_score(labels, km.labels_)
+        if score > best_score:
+            best_score = score
+            cluster_size = i
+            print("Current best Homogeneity Score (%.2f) for cluster size %i" % (best_score, cluster_size))
+
+    return cluster_size
+
+
+def show_top10(classifier, vectorizer, categories):
+    feature_names = np.asarray(vectorizer.get_feature_names())
+    for i, category in enumerate(categories):
+        top10 = np.argsort(classifier.coef_[i])[-10:]
+        print("%s: %s" % (category, " ".join(feature_names[top10])))
+
+
+def show_similar_posts():
+    # search for similar posts in the same category
+    similar_indices = (kmeans.labels_ == new_post_label).nonzero()[0]
+
+    similar = []
+    for i in similar_indices:
+        dist = np.linalg.norm((new_post_vec - X_train[i]).toarray())
+        similar.append((dist, train_data.data[i]))
+
+    similar = sorted(similar)
+
+    show_at_1 = similar[0]
+    show_at_2 = similar[len(similar) / 2]
+    show_at_3 = similar[-1]
+
+    category = train_data.target_names
+    print("Showing similar posts from category: '" + str(new_post_label) + "' for post: '" + str(new_post) + "'")
+    print(120 * '=')
+    print(show_at_1)
+    print(120 * '-')
+    print(show_at_2)
+    print(120 * '-')
+    print(show_at_3)
+    print(120 * '=')
+
+
 # the following things happen here:
 # 1. we tokenize the posts
 # 2. we throw away words that occur too often to be of any help by calculating tf–idf values
@@ -109,60 +176,9 @@ labels = train_data.target
 print("# training samples: %d, # training features: %d" % (num_train_samples, num_train_features))
 print("# test samples: %d, # test features: %d" % (num_test_samples, num_test_features))
 
-n_clusters = 50
-
-
-def bench_k_means(km, name, data):
-    print(120 * '=')
-    t0 = time()
-    km.fit(data)
-    print "Algorithm -- Time -- Homogeneity -- Completeness -- V-Measure -- Adjusted Rand Index -- Adjusted Mutual Info -- Silhouette Coefficient"
-    print(
-        '% 9s    %.2fs   %.3f           %.3f            %.3f         %.3f                   %.3f                    %.3f'
-        % (name, (time() - t0),
-           metrics.homogeneity_score(labels, km.labels_),
-           metrics.completeness_score(labels, km.labels_),
-           metrics.v_measure_score(labels, km.labels_),
-           metrics.adjusted_rand_score(labels, km.labels_),
-           metrics.adjusted_mutual_info_score(labels, km.labels_),
-           metrics.silhouette_score(data, km.labels_,
-                                    metric='euclidean',
-                                    sample_size=1000)))
-    print(120 * '=')
-
-
-def show_top10(classifier, vectorizer, categories):
-    feature_names = np.asarray(vectorizer.get_feature_names())
-    for i, category in enumerate(categories):
-        top10 = np.argsort(classifier.coef_[i])[-10:]
-        print("%s: %s" % (category, " ".join(feature_names[top10])))
-
-
-def show_similar_posts():
-    # search for similar posts in the same category
-    similar_indices = (kmeans.labels_ == new_post_label).nonzero()[0]
-
-    similar = []
-    for i in similar_indices:
-        dist = np.linalg.norm((new_post_vec - X_train[i]).toarray())
-        similar.append((dist, train_data.data[i]))
-
-    similar = sorted(similar)
-
-    show_at_1 = similar[0]
-    show_at_2 = similar[len(similar) / 2]
-    show_at_3 = similar[-1]
-
-    category = train_data.target_names
-    print("Showing similar posts from category: '" + str(new_post_label) + "' for post: '" + str(new_post) + "'")
-    print(120 * '=')
-    print(show_at_1)
-    print(120 * '-')
-    print(show_at_2)
-    print(120 * '-')
-    print(show_at_3)
-    print(120 * '=')
-
+n_clusters = 46
+#best_cluster_size = determine_best_cluster_size(30, 50, X_train)
+#print("Best cluster size for KMeans++ would be: " + str(best_cluster_size))
 
 kmeans = KMeans(init='random', n_clusters=n_clusters, n_init=10)
 bench_k_means(kmeans, name="random", data=X_train)
